962d1b5a1a8ad8d6602d44fd8da1ff9bcd4b0763
Watson,Kelvin <kelvinwatson@gmail.com>
2023-08-20 22:59:32 -0700

    custom collapsing toolbar draft

--- app/src/main/java/com/determinasian/outandaboutevents/ui/components/eventslist/EventsList.kt
+++ app/src/main/java/com/determinasian/outandaboutevents/ui/components/eventslist/EventsList.kt
@@ -1,9 +1,14 @@
 package com.determinasian.outandaboutevents.ui.components.eventslist
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun EventsList() {
-    Text("hello list")
+    LazyColumn {
+        items(100) {
+            Text("hello list")
+        }
+    }
 }
