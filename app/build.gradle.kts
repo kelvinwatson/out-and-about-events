dfed6807be7057863525a8bf7bf6b5b49e1d9e10
Watson,Kelvin <kelvinwatson@gmail.com>
2023-08-21 18:43:22 -0700

    fix splash screen icon - new image asset --> import svg --> move the svg from mipmap to drawable and use that for the splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.determinasian.outandaboutevents"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
